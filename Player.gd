89a31509e13d79c0ebc93e9fb9d781039e2fff2d
coreglitch <mail@s3f.ru>
2024-04-13 14:01:06 +0400

    click n run

--- Player.gd
+++ Player.gd
@@ -2,9 +2,24 @@ extends Node2D
 
 const SPEED = 800
 
+var target_position = Vector2()
+
 func _ready() -> void:
-	pass
+	target_position = position
 
 func _process(delta: float) -> void:
+	# handle keyboard movement
 	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	position += input * SPEED * delta
+	if input.length() > 0:
+		position += input * SPEED * delta
+		target_position = position
+	
+	# handle mouse click
+	if Input.is_action_just_pressed("ui_click"):
+		var click_position = get_global_mouse_position()
+		if position.distance_to(click_position) > 1:
+			target_position = click_position
+	
+	# handle mouse movement
+	if position.distance_to(target_position) > 10:
+		position += (target_position - position).normalized() * SPEED * delta
