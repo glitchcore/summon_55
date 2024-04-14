519be704e79e52e4c62604b034e05d36dcaa3ca8
coreglitch <mail@s3f.ru>
2024-04-14 10:56:46 +0400

    player animation

--- Man.gd
+++ Man.gd
@@ -7,11 +7,5 @@ onready var game = get_node("/root/Game") as Node
 func _ready() -> void:
 	var _r = self.connect("player_collide", game, "on_PlayerCollide")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-
-
 func _on_Area2D_body_entered(body: Node) -> void:
 	emit_signal("player_collide", Vector2(), "example")
